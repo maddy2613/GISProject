7d55a4b56039d5b7f5cc4cba3aca93c26fcea562
maddy2613 <48627410+maddy2613@users.noreply.github.com>
2024-07-02 13:19:57 -0500

    nearmeAnalysis.py

--- nearmeAnalysis.py
+++ nearmeAnalysis.py
@@ -5,7 +5,7 @@ import arcpy
 from arcgis.geometry import Geometry, project
 import datetime
 
-log_path = r"C:\Users\srv\Downloads\test\Logs"
+log_path = r"C:\Users\l7bw\OneDrive - PGE\Desktop\Near Analysis\Logs"
 logger = logging.getLogger("LoggerName")
 log_format = "%(asctime)s - %(levelname)s - %(message)s"
 log_level = logging.INFO
@@ -13,13 +13,13 @@ filename = log_path + datetime.datetime.now().strftime('%Y_%m_%d.log')
 logging.basicConfig(filename=filename, format=log_format,
                     level=log_level)
 logger.info('process started')
-in_fc =""
-nearme_fc = ""
+in_fc =r"C:\Users\l7bw\OneDrive - PGE\Desktop\Prod Data Append 0617.gdb\FTI_SpanInspection_QC_Not_Ready_0617"
+nearme_fc =r"C:\Users\l7bw\OneDrive - PGE\Desktop\Prod Data Append 0617.gdb\FTI_TreePrescription_QC_Not_Ready_0617"
 nearest_dict = {}
-with arcpy.da.SearchCursor(in_fc, ["OID@", "NEAR_FID","NEAR_FID"]) as rows:
+with arcpy.da.SearchCursor(in_fc, ["OID", "SPAN_ID","AuditId"]) as rows:
     for row in rows:
         try:
-            nearest_dict[row[0]].append({"field1":row[1],"field2":row[2]})
+            nearest_dict[row[0]] = {"field1":row[1],"field2":row[2]}
         except Exception as err:
             logger.error('error preparing obj {}'.format(err))
             print('error preparing obj {}'.format(err))
@@ -27,7 +27,7 @@ with arcpy.da.SearchCursor(in_fc, ["OID@", "NEAR_FID","NEAR_FID"]) as rows:
 print(nearest_dict)
 try:
     search_radius = "500 DecimalDegrees"
-    arcpy.analysis.Near(in_fc, nearme_fc, search_radius, "NO_LOCATION", "NO_ANGLE","PLANAR","NEAR_FID NEAR_FID; NEAR_DIST NEAR_DIST")
+    arcpy.analysis.Near(nearme_fc,in_fc, search_radius, "NO_LOCATION", "NO_ANGLE","PLANAR","NEAR_FID NEAR_FID; NEAR_DIST NEAR_DIST")
 except arcpy.ExecuteError:
     print(arcpy.GetMessages(2))
 except Exception as err2:
@@ -35,10 +35,10 @@ except Exception as err2:
 print(print(arcpy.GetMessages()))
 
 logger.info('calculating SPANID and Audit Id fields')
-fields = ['NEAR_FID','ROAD_TYPE', 'BUFFER_DISTANCE']
+fields = ['NEAR_FID','SPAN_ID', 'AuditID']
 
 # Create update cursor for feature class
-with arcpy.da.UpdateCursor(in_fc, fields) as cursor:
+with arcpy.da.UpdateCursor(nearme_fc, fields) as cursor:
     for row in cursor:
         try:
             cal_fields =  nearest_dict[row[0]]
@@ -49,9 +49,3 @@ with arcpy.da.UpdateCursor(in_fc, fields) as cursor:
             logger.info('failed while updating fileds')
             print('failed while updating fileds')
             continue
-
-
-
-
-
-
